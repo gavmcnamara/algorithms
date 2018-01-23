73348443beabec5c9cc88c905f0495fc1809dc67
Gavin McNamara <gavin.mcnamara01@myhunter.cuny.edu>
2018-01-23 01:28:03 -0500

    slightly modified code and comments

--- python/anagram.py
+++ python/anagram.py
@@ -1,7 +1,7 @@
 '''
  Given two strings s and t, determine whether some anagram
- of t is a substring of s. For example: if s = "udacity"
- and t = "ad", then the function returns True.
+ of t is a substring of s. For example: if s = "palendrome"
+ and t = "end", then the function returns True.
  Your function definition should look
  like: question1(s, t) and return a boolean True or False.
 
@@ -9,19 +9,19 @@
  '''
 
 def isAnagram(str1, str2):
-    # Creates 2 sorted lists
+    # creates 2 lists and compares and returns sorted list
     str1_list = sorted(str1)
     str2_list = sorted(str2)
-    # Compares and returns the sorted lists
-    return (str1_list==str2_list)
+    return (str1_list == str2_list)
 
 def question1(s, t):
-    # Iterates to see if t is a substring of s
     for i in range(len(s)-len(t)+1):
+        # if t is a substring of s return True
         if isAnagram(s[i: i+len(t)], t):
             return True
     return False
 
+print(question1("palendrome", "end"))
 print(question1("gavin", "vin"))
 print(question1("gavin", "asd"))
 
