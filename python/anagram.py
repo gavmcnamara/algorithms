9e889eedc6fcb2be19533b46b0cb3f250342a3b7
Gavin McNamara <gavin.mcnamara01@myhunter.cuny.edu>
2018-01-22 23:26:26 -0500

    created functions isAnagram and Question1

---
+++ python/anagram.py
@@ -0,0 +1,23 @@
+'''
+ Given two strings s and t, determine whether some anagram
+ of t is a substring of s. For example: if s = "udacity"
+ and t = "ad", then the function returns True.
+ Your function definition should look
+ like: question1(s, t) and return a boolean True or False.
+
+ Takes in 2 aruements and checks if they are anagrams
+ '''
+
+def isAnagram(str1, str2):
+    str1_list = sorted(str1)
+    str2_list = sorted(str2)
+    return (str1_list==str2_list)
+
+def question1(s, t):
+    for i in range(len(s)-len(t)+1):
+        if isAnagram(s[i: i+len(t)], t):
+            return True
+    return False
+
+print(question1("gavin", "vin"))
+print(question1("gavin", "asd"))
