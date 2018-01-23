0604b81806e3224c2657b932f3e4929a724c9d51
Gavin McNamara <gavin.mcnamara01@myhunter.cuny.edu>
2018-01-23 00:08:36 -0500

    created function that takes string and returns longest palendrome

--- python/palendrome.py
+++ python/palendrome.py
@@ -1,3 +1,28 @@
 ''' Given a substring a, find the longest palendromic
 substring contained in a.
 '''
+
+def question2(a):
+    # if a has no values there is no runtime error
+    if a <= "":
+        return a
+
+    lng_palendrome = ""
+    for i in range(len(a)):
+        for x in range(i):
+            substring = a[x: i+1]
+            if substring == substring[:: -1]:
+                if len(substring) > len(lng_palendrome):
+                    lng_palendrome = substring
+
+    if len(lng_palendrome) == 0:
+        print(a, "is not a palendrome")
+    return lng_palendrome
+
+print(question2("racecar"))
+print(question2("dabbac"))
+
+
+
+
+
