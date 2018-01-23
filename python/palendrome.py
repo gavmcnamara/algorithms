db66b2fbe5daa7d0b92a8ca84565ed6968d08e21
Gavin McNamara <gavin.mcnamara01@myhunter.cuny.edu>
2018-01-23 01:28:46 -0500

    slightly modified code and comments

--- python/palendrome.py
+++ python/palendrome.py
@@ -3,25 +3,27 @@ substring contained in a.
 '''
 
 def question2(a):
-    # if a has no values there is no runtime error
+    # if a is nothing there will be no runtime errors
     if a <= "":
         return a
 
+    # creates string that will contain longest palendrome
     lng_palendrome = ""
     for i in range(len(a)):
         for x in range(i):
             substring = a[x: i+1]
-            if substring == substring[:: -1]:
+            # checks is substring contains a palendrome
+            if substring == substring[::-1]:
                 if len(substring) > len(lng_palendrome):
                     lng_palendrome = substring
 
     if len(lng_palendrome) == 0:
-        print(a, "is not a palendrome")
+        print a, "is not a palendrome!"
     return lng_palendrome
 
-print(question2("racecar"))
-print(question2("dabbac"))
-
+print question2("racecar")
+print question2("baac")
+print question2("asdf")
 '''Efficiency: O(n^2), this is because of the two for loops
 the first for loop is equal to O(n) and the second is
 equal to O(n). So combined it is O(n^2).
