ba567d7d113ef163a3bf5f006a5d57c1f55d00ce
Gavin McNamara <gavin.mcnamara01@myhunter.cuny.edu>
2018-03-06 21:28:45 -0500

    created file that will run a song when I want to take a break

---
+++ python/take_a_break.py
@@ -0,0 +1,13 @@
+import webbrowser
+import time
+
+
+total_breaks = 3
+break_count = 0
+
+print("This program started! " + time.ctime())
+while(break_count < total_breaks):
+    time.sleep(5)
+    webbrowser.open("https://www.youtube.com/watch?v=h-_NNIX8cDA")
+    break_count = break_count + 1
+
