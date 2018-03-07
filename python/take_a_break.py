f4639de5ea8f7d6b985fb7ca712d6690b3c6962c
Gavin McNamara <gavin.mcnamara01@myhunter.cuny.edu>
2018-03-06 21:42:04 -0500

    added second time option in comments

--- python/take_a_break.py
+++ python/take_a_break.py
@@ -7,7 +7,8 @@ break_count = 0
 
 print("This program started! " + time.ctime())
 while(break_count < total_breaks):
-    time.sleep(5)
+    time.sleep(10)
+    #time.sleep(2*60*60) This would be for every 2 hours
     webbrowser.open("https://www.youtube.com/watch?v=h-_NNIX8cDA")
     break_count = break_count + 1
 
